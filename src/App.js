7bdb189c15f3b084e88601c81db585984ce53874
Dillon Awes <dillonawes@gmail.com>
2023-11-28 17:19:40 -0600

    counter app increment and count functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { React, useState } from "react";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter />
     </div>
   );
 }
 
-export default App;
+function Counter() {
+  const [count, setCount] = useState(0);
+  const handlePlus = () => setCount((c) => c + step);
+  const handleMinus = () => setCount((c) => c - step);
+  const date = new Date();
+  date.setDate(date.getDate() + count);
+  const [step, setStep] = useState(1);
+  const handleStepMinus = () => setStep((s) => s - 1);
+  const handleStepPlus = () => setStep((s) => s + 1);
+  return (
+    <>
+      <div>
+        <div className="step">
+          <button className="stepMinus" onClick={handleStepMinus}>
+            -
+          </button>
+          <span>Step: {step}</span>
+          <button className="stepPlus" onClick={handleStepPlus}>
+            +
+          </button>
+        </div>
+
+        <div className="count">
+          <button className="minus" onClick={handleMinus}>
+            -
+          </button>
+          <span>Count: {count}</span>
+          <button className="plus" onClick={handlePlus}>
+            +
+          </button>
+        </div>
+
+        <div>
+          <span>
+            {count === 0
+              ? "Today is "
+              : count > 0
+              ? `${count} days until `
+              : `${Math.abs(count)} days ago was `}
+          </span>
+          <span>{date.toDateString()}</span>
+        </div>
+      </div>
+    </>
+  );
+}
